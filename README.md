29fa2ddbe2b39700941f35b60dd3360473e1f3bf
dikey0ficial <79040270950@ya.ru>
2022-01-24 21:40:32 +0300

    App mode, TOML configs

--- README.md
+++ README.md
@@ -1,17 +1,40 @@
-# DLFM
-*it was a fork of [DiscordLastfmScrobbler](https://github.com/edvardpotter/DiscordLastfmScrobbler)*
-### DOWNLOAD [HERE](https://github.com/dikey0ficial/dlfm/releases "Releases")
+# dlfm
+## This app will update your Discord status with information about song that you scrobble now
 
-This app will update your Discord "Listening to" status with when you are scrobbling something to Last.FM
+<details><summary><b>How to install</b></summary>
 
-![Profile screenshot](https://i.imgur.com/SbtvFJa.png "Profile screenshot") *profile screenshot*
+You can download a compiled version from [releases pages](https://github.com/dikey0ficial/dlfm/releases)
 
+If you have go installed, you can only write this in console:
+`go install github.com/dikey0ficia/dlfm`
 
-![Userlist screenshot](https://i.imgur.com/x5mWIXR.png "Screenshot from list of users") *screenshot from list of users*
+</details>
 
+<details><summary><b>Screenshots</b></summary>
 
+#### Token mode:
+Profile screenshot:
 
-To run it, you need to edit config and complete three things:
+![Profile screenshot](https://i.imgur.com/SbtvFJa.png "Profile screenshot")
+
+Screenshot from list of users:
+
+![Screenshot from list of users](https://i.imgur.com/x5mWIXR.png "Screenshot from list of users") 
+
+#### App mode:
+
+Profile screenshot:
+
+![Profile screenshot](https://i.imgur.com/shI5JJT.png "Profile screenshot")
+
+Screenshot from list of users:
+
+![Screenshot from list of users](https://i.imgur.com/F5E1GPz.png "Screenshot from list of users")
+</details>
+
+Config file (config.toml) have three parts: last.fm, discord and application settings.
+Here are instruction to fill it correctly
+
 ## **1. Last.FM API Key**
 
 Go to the [Last.FM API Page](https://www.last.fm/api/account/create) and sign in with your existing Last.FM username and password. It should bring you to the **Create API account** page and ask you for a few things.
@@ -24,57 +47,104 @@ After clicking Submit you should get a confirmation page with two items: *API Ke
 
 ![LastFM API Account Created Screenshot](https://i.imgur.com/1Qb7LeO.png "don't ask why names aren't same")
 
-Copy and paste the API Key value into the config file in the `api_key = xxx` line
+Copy and paste the API Key value into the config file in the `api_key = xxx` line **with quotes**
 
-Then, fill field `username` with your last.fm username
+Then, fill field `username` with your last.fm username **with quotes**
 
-Also you should fill field `check_interval` with integer number of seconds script should check new tracks. Recomended values: 1-5 seconds (if you are listening to grindcore, write 0)
+Also you should fill field `check_interval` with integer number of seconds script should check new tracks. Recomended values: 5-15 seconds (if you are listening to grindcore, write 0). **Without quotes.**
 
 ## **2. Discord User Token**
 
-For this one you'll need to use the desktop app - it won't work on mobile or in web.
+Firstly, you should fill `use_app_mode` field with `true` or `false` (without quotes). By default it's `false` — token mode.
 
-If you are using the desktop app:
+<details><summary>What is the difference?</summary>
 
-- Press **Ctrl+R** (or **Cmd+R** on Mac)
-- Click the "*Application*" tab
-- Click and expand the "*Local Storage*" section
-- Click on the only entry in this section, "*https://discord.com/*"
-- Press **Ctrl+Shift+I** (or **Cmd+Shift+I** on Mac)
-- Wait few seconds
-- Right click -> Edit Value in the field to the right of "*token*"
-- Copy and paste the token value into the config file on the `token = xxx` line and remove the quotation marks from it.
+| Token mode                  |               App mode |
+|:----------------------------|-----------------------:|
+| Using user token (unsafe)   |   Using application ID |
+| Hard to get token           |         Easy to get ID |
+| "Listening to ..."          |          "Playing ..." |
+| Custom title                |         Title is fixed |
+| No images                   | Large and small images |
+| No image texts              |     Custom image texts |
+| Can't work with text status | Works with text status |
+</details>
+
+Next step is different in App mode and Token mode, so I placed it in spoiler
+
+<details><summary>Token mode</summary>
+
+1. Go to Discord app
+2. Press **Ctrl+R** (or **Cmd+R** on Mac)
+3. Click the "*Application*" tab
+4. Click and expand the "*Local Storage*" section
+5. Click on the only entry in this section, *"https://discord.com/"*
+6. Press **Ctrl+Shift+I** (or **Cmd+Shift+I** on Mac)
+7. Wait few seconds
+8. Right click -> Edit Value in the field to the right of "*token*"
+9. Copy and paste the token value into the config file on the `token = discordtoken` line **saving quotes**.
 
 ![Desktop Token](https://i.imgur.com/sPs0New.png)
+</details>
+
+<details><summary>App mode</summary>
+
+1. Go to [Discord Developer Portal's Applications page](https://discord.com/developers/applications "link")
+2. Click to *"New application"* button
+3. Type the name you want to see as title in your status (for example, type "qwe42" to see "Plaing qwe42" status)
+4. Copy and paste *Application ID* into `app_id = 0123456789101112` line. **Delete quotes**!
+
+![New application](https://i.imgur.com/Qd85IeE.png)
+
+![Application ID](https://i.imgur.com/qphnFDa.png)
+</details>
 
 ## **3. App settings**
 
-You can change there *title* and *endless* options.
+If you want to ignore not critic errors, change "*endless_mode*" to `true`, else fill it with `false`. **Without quotes**!
 
-If you want to change title of status (for example, "Listening to Deezer" or "Listening to Youtube Music"), change "title" value
-Example:
+Now you can modify Title in Token mode and Large image, Large text (what you'll see if you cover large image),
+Small image and Small text (what you'll se if you cover small image) in App mode
 
-![Example of changed title](https://i.imgur.com/9OShK3U.png)
-In title you can use tags, which will be replaced with other value.
-
+In title, large image, large text, small text you can use these tags — parts of text that will be changed.
 List of tags:
 
-| Tag         |             Replaced with |
-|:------------|--------------------------:|
-| {{artist}}  | Track's artist(s) name(s) |
-| {{album}}   | Track's album name        |
-| {{name}}    | Track name                |
+| Tag             |                     Value |    Type    |
+|:----------------|---------------------------|-----------:|
+| {{artist}}      | Track's artist(s) name(s) |    text    |
+| {{album}}       | Track's album name        |    text    |
+| {{name}}        | Track name                |    text    |
+| {{album_image}} | Album cover from last.fm  |    image   |
 
-Also if you want to ignore not critic errors, change "*endless_mode*" to "true", else fill it with "false"
+For small image you can use other tags — they will be changed with icon of service:
+
+<details><summary>Tags and images (too big to show without spoiler)</summary>
+
+| Tag             |                                                                                                 Icon |
+|:----------------|-----------------------------------------------------------------------------------------------------:|
+| {{lastfm}}      | ![](http://icons.iconarchive.com/icons/danleech/simple/512/lastfm-icon.png)                          |
+| {{deezer}}      | ![](https://www.macupdate.com/images/icons512/60905.png)                                             |
+| {{youtube}}     | ![](https://seeklogo.com/images/Y/youtube-music-logo-50422973B2-seeklogo.com.png)                    |
+| {{apple}}       | ![](http://ixd.prattsi.org/wp-content/uploads/2017/01/apple_music_logo_by_mattroxzworld-d982zrj.png) |
+| {{vk}}          | ![](https://seeklogo.com/images/V/vk-icon-logo-10188561D5-seeklogo.com.png)                          |
+| {{yandex}}      | ![](https://download.cdn.yandex.net/from/yandex.ru/support/ru/music/files/icon_main.png)             |
+| {{soundcloud}}  | ![](https://icons.iconarchive.com/icons/sicons/basic-round-social/512/soundcloud-icon.png)           |
+
+*icons aren't mine, i just tooked their URLs*
+</details>
+
+
+You can also fill `large_image` and `small_image` with your own URLs to any picture
+
 
 ## When you're done
 
-Save your config file as "*config.ini*" and it should look something like this:
+Save your config file as "*config.toml*" and it should look something like this:
 
-![Finished Config File](https://i.imgur.com/vP1WXeD.png)
+![Finished Config File](https://i.imgur.com/JVJOVy8.png)
 
-Now just run the executable. It should connect to Discord and immediately start setting your "*Playing*" status to whatever you're listening to on Last.FM
+Now just run the executable. It should connect to Discord and immediately start setting your status to whatever you're listening to on last.fm
 
-If it's working, it will look like this:
+If it's working, console will look like this:
 
-![Running Executable](https://i.imgur.com/S2LehkW.png)
+![Running Executable](https://i.imgur.com/uDjruCs.png)
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/bwmarrin/discordgo"
-	"github.com/go-ini/ini"
+	"github.com/BurntSushi/toml"
+	dgo "github.com/bwmarrin/discordgo"
+	rgo "github.com/hugolgst/rich-go/client"
 	"github.com/shkh/lastfm-go/lastfm"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
@@ -14,6 +17,113 @@ import (
 	"time"
 )
 
+var conf = struct {
+	LastFm struct {
+		APIKey        string `toml:"api_key"`
+		Username      string `toml:"username"`
+		CheckInterval int    `toml:"check_interval"`
+	} `toml:"lastfm"`
+	Discord struct {
+		UseAppMode bool   `toml:"use_app_mode"`
+		Token      string `toml:"token"`
+		AppID      int    `toml:"app_id"`
+	} `toml:"discord"`
+	App struct {
+		Title       string `toml:"title"`
+		LargeImage  string `toml:"large_image"`
+		LargeText   string `toml:"large_text"`
+		SmallImage  string `toml:"small_image"`
+		SmallText   string `toml:"small_text"`
+		EndlessMode bool   `toml:"endless_mode"`
+	} `toml:"app"`
+}{}
+
+func init() {
+	f, err := os.Open("config.toml")
+	if err != nil {
+		log.Println("Error loading config:", err)
+		end(nil)
+		os.Exit(1)
+	}
+	data, err := ioutil.ReadAll(f)
+	if err != nil && err != io.EOF {
+		log.Println("Error loading config:", err)
+		end(nil)
+		os.Exit(1)
+	}
+	if err := toml.Unmarshal(data, &conf); err != nil {
+		log.Println("Error loading config:", err)
+		end(nil)
+		os.Exit(1)
+	}
+	log.Println("Config loaded")
+	var (
+		noErr bool
+		strs  = []*string{
+			&conf.LastFm.APIKey, &conf.LastFm.Username,
+			&conf.Discord.Token, &conf.App.Title,
+			&conf.App.LargeImage, &conf.App.LargeText,
+			&conf.App.SmallImage, &conf.App.SmallText,
+		}
+	)
+	for _, v := range strs {
+		*v = strings.TrimSpace(*v)
+	}
+	if conf.LastFm.APIKey == "" {
+		log.Println("Error: lastfm.api_key can't be empty")
+	} else if conf.LastFm.Username == "" {
+		log.Println("Error: lastfm.username can't be empty")
+	} else if conf.Discord.Token == "" && !conf.Discord.UseAppMode {
+		log.Println("Error: discord.token can't be empty in token mode")
+	} else if conf.Discord.AppID == 0 && conf.Discord.UseAppMode {
+		log.Println("Error: discord.app_id can't be empty in app mode")
+	} else if conf.LastFm.CheckInterval < 1 {
+		if conf.LastFm.CheckInterval == 0 {
+			log.Println("Grindcore mode on!")
+			noErr = true
+		} else {
+			log.Println("Error: invalid check_interval (< 0)")
+		}
+	} else {
+		noErr = true
+	}
+	if !noErr {
+		end(nil)
+		os.Exit(1)
+	}
+	if conf.App.Title == "" {
+		conf.App.Title = "last.fm"
+	}
+	if conf.App.LargeImage == "" {
+		conf.App.LargeImage = "{{album_image}}"
+	}
+	if conf.App.LargeText == "" {
+		conf.App.LargeText = "{{album}}"
+	}
+	if conf.App.SmallImage == "" {
+		conf.App.SmallImage = "{{lastfm}}"
+	}
+	if conf.App.SmallText == "" {
+		conf.App.SmallText = "github.com/dikey0ficial/dlfm"
+	}
+	for name, url := range map[string]string{
+		// got this URLs from search, they aren't mine
+		// and can be with any license (i haven't checked)
+		"lastfm":     "http://icons.iconarchive.com/icons/danleech/simple/512/lastfm-icon.png",
+		"deezer":     "https://www.macupdate.com/images/icons512/60905.png",
+		"youtube":    "https://seeklogo.com/images/Y/youtube-music-logo-50422973B2-seeklogo.com.png",
+		"apple":      "http://ixd.prattsi.org/wp-content/uploads/2017/01/apple_music_logo_by_mattroxzworld-d982zrj.png",
+		"vk":         "https://seeklogo.com/images/V/vk-icon-logo-10188561D5-seeklogo.com.png",
+		"yandex":     "https://download.cdn.yandex.net/from/yandex.ru/support/ru/music/files/icon_main.png",
+		"soundcloud": "https://icons.iconarchive.com/icons/sicons/basic-round-social/512/soundcloud-icon.png",
+	} {
+		if conf.App.SmallImage == "{{"+name+"}}" {
+			conf.App.SmallImage = url
+			break
+		}
+	}
+}
+
 func end(err error) {
 	if err != nil {
 		log.Println(err)
@@ -22,63 +132,103 @@ func end(err error) {
 	fmt.Scanln()
 }
 
+func tokenClear(dg *dgo.Session) error {
+	return dg.UpdateStatusComplex(dgo.UpdateStatusData{Game: nil})
+}
+
+type lrt = lastfm.UserGetRecentTracks
+
+func appClear(_ *dgo.Session) error {
+	return rgo.SetActivity(rgo.Activity{})
+}
+
+func tokenSet(dg *dgo.Session, r lrt) error {
+	ctrack := r.Tracks[0]
+	ftitle := conf.App.Title
+	ftitle = strings.Replace(ftitle, "{{name}}", ctrack.Name, -1)
+	ftitle = strings.Replace(ftitle, "{{artist}}", ctrack.Artist.Name, -1)
+	ftitle = strings.Replace(ftitle, "{{album}}", ctrack.Album.Name, -1)
+	return dg.UpdateStatusComplex(
+		dgo.UpdateStatusData{
+			Game: &dgo.Game{
+				Name:    ftitle,
+				Type:    2,
+				Details: ctrack.Name,
+				State:   ctrack.Artist.Name,
+			},
+		},
+	)
+}
+
+func appSet(_ *dgo.Session, r lrt) error {
+	ctrack := r.Tracks[0]
+	fltext, fstext := conf.App.LargeText,
+		conf.App.SmallText
+	for _, v := range []*string{&fltext, &fstext} { // texts
+		*v = strings.Replace(*v, "{{name}}", ctrack.Name, -1)
+		*v = strings.Replace(*v, "{{artist}}", ctrack.Artist.Name, -1)
+		*v = strings.Replace(*v, "{{album}}", ctrack.Album.Name, -1)
+	}
+	flimg := conf.App.LargeImage
+	flimg = strings.Replace(flimg, "{{album_image}}", ctrack.Images[3].Url, -1)
+	return rgo.SetActivity(
+		rgo.Activity{
+			Details:    ctrack.Name,
+			State:      ctrack.Artist.Name,
+			LargeImage: flimg,
+			LargeText:  fltext,
+			SmallImage: conf.App.SmallImage,
+			SmallText:  fstext,
+		},
+	)
+}
+
 func main() {
-	cfg, err := ini.Load("config.ini")
-	if err != nil {
-		end(err)
-		return
-	}
+	api := lastfm.New(conf.LastFm.APIKey, "")
 
-	trim := strings.TrimSpace
-	token := trim(cfg.Section("discord").Key("token").String())
-	apiKey := trim(cfg.Section("lastfm").Key("api_key").String())
-	username := trim(cfg.Section("lastfm").Key("username").String())
-	title := trim(cfg.Section("app").Key("title").String())
-	endlessMode, err := strconv.ParseBool(cfg.Section("app").Key("endless_mode").String())
+	log.Println("Connected to last.fm")
 
-	if err != nil {
-		end(err)
-		return
-	}
-	configInterval, err := cfg.Section("lastfm").Key("check_interval").Int()
-
-	if err != nil {
-		end(err)
-		return
-	}
-
-	api := lastfm.New(apiKey, "")
-
-	log.Println("Settings loaded: config.ini")
-
-	if endlessMode {
+	if conf.App.EndlessMode {
 		log.Println("Endless mode! Press `Ctrl+C` to exit")
 	}
-	dg, err := discordgo.New(token)
-	if err != nil {
-		end(err)
-		return
+	var (
+		dg  *dgo.Session
+		err error
+	)
+	if conf.Discord.UseAppMode {
+		if err = rgo.Login(strconv.Itoa(conf.Discord.AppID)); err != nil {
+			end(err)
+			return
+		}
+		defer rgo.Logout()
+	} else {
+		if dg, err = dgo.New(conf.Discord.Token); err != nil {
+			end(err)
+			return
+		} else if err = dg.Open(); err != nil {
+			end(err)
+			return
+		}
+		defer dg.Close()
 	}
 
-	log.Println("Authorized to Discord")
+	log.Println("Authorized and connected to discord")
 
-	if err := dg.Open(); err != nil {
-		end(err)
-		return
-	}
-
-	defer dg.Close()
-	log.Println("Connected to Discord")
-
-	interval := time.Duration(configInterval*1000) * time.Millisecond
-	ticker := time.NewTicker(interval)
+	var set, clear = func() (
+		func(*dgo.Session, lrt) error,
+		func(*dgo.Session) error) {
+		if conf.Discord.UseAppMode {
+			return appSet, appClear
+		}
+		return tokenSet, tokenClear
+	}()
 
 	var deathChan = make(chan os.Signal, 0)
 	signal.Notify(deathChan, os.Interrupt, syscall.SIGTERM)
-	go func(dg *discordgo.Session, deatchChan chan os.Signal) {
+
+	go func(deathChan chan os.Signal) {
 		<-deathChan
-		statusData := discordgo.UpdateStatusData{Game: nil}
-		if err := dg.UpdateStatusComplex(statusData); err != nil {
+		if err := clear(dg); err != nil {
 			log.Println("Error during deleting status:", err)
 			end(nil)
 			return
@@ -92,44 +242,35 @@ func main() {
 		log.Println("Deleted status!")
 		end(nil)
 		os.Exit(0)
-	}(dg, deathChan)
+	}(deathChan)
+
+	interval := time.Duration(conf.LastFm.CheckInterval) * time.Second
+	ticker := time.NewTicker(interval)
+
 	var (
 		prevTrack string
-		// fTitle is f(ull) title
-		fTitle string
 	)
+
 	for {
 		select {
 		case <-ticker.C:
-			result, err := api.User.GetRecentTracks(lastfm.P{"limit": "1", "user": username})
+			result, err := api.User.GetRecentTracks(lastfm.P{"limit": "1",
+				"user": conf.LastFm.Username})
 			if err != nil {
-				log.Println("LastFM error: ", err)
-				if !endlessMode {
+				log.Println("last.fm error: ", err)
+				if !conf.App.EndlessMode {
 					end(nil)
 					return
 				}
 			} else {
 				if len(result.Tracks) > 0 {
-					currentTrack := result.Tracks[0]
-					isNowPlaying, _ := strconv.ParseBool(currentTrack.NowPlaying)
-					trackName := currentTrack.Artist.Name + " - " + currentTrack.Name
+					ctrack := result.Tracks[0]
+					isNowPlaying, _ := strconv.ParseBool(ctrack.NowPlaying)
+					trackName := ctrack.Artist.Name + " - " + ctrack.Name
 					if isNowPlaying {
-						fTitle = strings.Replace(title, "{{name}}", currentTrack.Name, -1)
-						fTitle = strings.Replace(fTitle, "{{artist}}", currentTrack.Artist.Name, -1)
-						fTitle = strings.Replace(fTitle, "{{album}}", currentTrack.Album.Name, -1)
-						statusData := discordgo.UpdateStatusData{
-							Game: &discordgo.Game{
-								Name:    fTitle,
-								Type:    discordgo.GameTypeListening,
-								Details: currentTrack.Name,
-								State:   currentTrack.Artist.Name,
-							},
-							AFK:    false,
-							Status: "online",
-						}
-						if err := dg.UpdateStatusComplex(statusData); err != nil {
+						if err := set(dg, result); err != nil {
 							log.Println("Discord error: ", err)
-							if !endlessMode {
+							if !conf.App.EndlessMode {
 								end(nil)
 								break
 							}
@@ -139,13 +280,9 @@ func main() {
 							prevTrack = trackName
 						}
 					} else if !isNowPlaying {
-						statusData := discordgo.UpdateStatusData{
-							Game:   nil,
-							Status: "offline",
-						}
-						if err := dg.UpdateStatusComplex(statusData); err != nil {
+						if err := clear(dg); err != nil {
 							log.Println("Discord error: ", err)
-							if !endlessMode {
+							if !conf.App.EndlessMode {
 								end(nil)
 								break
 							}
